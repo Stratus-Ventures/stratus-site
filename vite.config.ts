3c945496021e2f204a99765f17bbb4185b1f72f7
jasoncoawette <jason.coawette@gmail.com>
2025-09-15 09:39:37 -0700

    Enhance Vite configuration and preload font assets
    - Updated vite.config.ts to include server settings for Cross-Origin policies and asset handling.
    - Added font preloading links in app.html for improved loading performance of custom fonts.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		fs: {
+			allow: ['..']
+		},
+		headers: {
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'Cross-Origin-Opener-Policy': 'same-origin'
+		}
+	},
+	build: {
+		assetsInlineLimit: 0
+	}
 });
