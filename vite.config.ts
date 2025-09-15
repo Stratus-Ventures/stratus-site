205a7965aee255bb5a98ef56d526a2b5ca82f720
jasoncoawette <jason.coawette@gmail.com>
2025-09-15 09:45:08 -0700

    Refactor Vite configuration and clean up app.html
    - Removed server settings and build configurations from vite.config.ts for simplification.
    - Eliminated font preloading links in app.html to streamline the HTML structure.

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		fs: {
-			allow: ['..']
-		},
-		headers: {
-			'Cross-Origin-Embedder-Policy': 'credentialless',
-			'Cross-Origin-Opener-Policy': 'same-origin'
-		}
-	},
-	build: {
-		assetsInlineLimit: 0
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
