75b609e2f1e846ed76a31ae42c1472cb4c605455
jasoncoawette <jason.coawette@gmail.com>
2025-10-14 11:31:47 -0700

    open graph

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 	
 	<!-- OpenGraph Meta Tags -->
 	<meta property="og:title" content="Stratus" />
-	<meta property="og:description" content="Stratus Ventures - Building the future of technology" />
+	<meta property="og:description" content="Stratus Ventures - Stratus Ventures LLC owns a group of AI-forward ventures founded by Jason Coawette and is located in Phoenix Arizona." />
 	<meta property="og:image" content={banner} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://stratus-ventures.org" />
@@ -22,7 +22,7 @@
 	<!-- Twitter Card Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="Stratus" />
-	<meta name="twitter:description" content="Stratus Ventures - Building the future of technology" />
+	<meta name="twitter:description" content="Stratus Ventures - Stratus Ventures LLC owns a group of AI-forward ventures founded by Jason Coawette and is located in Phoenix Arizona." />
 	<meta name="twitter:image" content={banner} />
 </svelte:head>
 
