3d46f33c8de8d9f95b032efdae786b629a36be9e
jasoncoawette <jason.coawette@gmail.com>
2025-10-14 17:32:29 -0700

    adjust tooltip

--- src/lib/components/metrics/Tooltip.svelte
+++ src/lib/components/metrics/Tooltip.svelte
@@ -15,7 +15,7 @@
 		class="
             small-medium absolute bottom-full z-10 mb-2 rounded-md bg-button-bg px-3 py-1.5
             whitespace-nowrap text-button-fg shadow-lg
-            {isFirst ? 'left-[1px] sm:left-1/2 sm:-translate-x-1/2' : 'left-1/2 -translate-x-1/2'}
+            {isFirst ? 'left-[1px] lg:left-1/2 lg:-translate-x-1/2' : 'left-1/2 -translate-x-1/2'}
         "
 		in:fly={{ y: 4, duration: 300 }}
 		out:fly={{ y: 4, duration: 300 }}
@@ -25,7 +25,7 @@
 			class="
                 absolute top-full h-3 w-3 rotate-45 rounded-br-[3px] bg-button-bg
                 {isFirst
-				? 'left-[5px] -translate-y-[7px] sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2'
+				? 'left-[5px] -translate-y-[7px] lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2'
 				: 'left-1/2 -translate-x-1/2 -translate-y-1/2'}
             "
 		></div>
